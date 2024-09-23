a709634fbe6d17640fb5172389c1dfeb3af011a1
marcos <marcosalvarezcalabria@gmail.com>
2024-09-23 19:18:12 +0100

    fixed manipulacion de array con punteros

--- manipulacion array con punteros.c
+++ manipulacion array con punteros.c
@@ -4,29 +4,25 @@ Tienes un array de enteros con valores positivos y negativos. Debes escribir un 
 Utilice un puntero para recorrer el array.
 Reemplace todos los valores negativos del array con el número 0, usando únicamente aritmética de punteros (es decir, no puedes usar índices de array como array[i]).
 Imprima el array modificado usando el puntero.*/
-#include <stdio.h>
 
-int main (void)
-{
+    #include <stdio.h>
+
+int main(void) {
     int arr[] = {1, 2, -5, 6};
-    int *arrPtr;
-    int index = 0;
-
-
-    arrPtr = arr;
+    int *ptrArr = arr;
+    int size = sizeof(arr) / sizeof(arr[0]); // Calcular el tamaño del array
+    int i = 0; // Contador para controlar el número de elementos
     
-    while(index > 0){
-        if(*arrPtr  < 0){
-            *arrPtr = 0;
+    while (i < size) {
+        if (*(ptrArr) < 0) {
+            *(ptrArr) = 0;
         }
-
-        arrPtr++;
-        index++;
-        printf("%d \n", *arrPtr);
+        
+        printf("%d ", *(ptrArr)); // Imprimir el valor del elemento actual
+        
+        ptrArr++; // Avanzar al siguiente elemento
+        i++; // Incrementar el contador
     }
 
-
-    
     return 0;
 }
-    
