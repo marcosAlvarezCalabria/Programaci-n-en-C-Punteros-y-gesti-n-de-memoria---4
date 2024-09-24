7c57887cb9f540c8ef668ee64c6e41e9d873d178
marcos <marcosalvarezcalabria@gmail.com>
2024-09-24 18:43:32 +0100

    add arrays amnd memory

--- arrays and functions.c
+++ arrays and functions.c
@@ -1,50 +1,50 @@
 #include <stdio.h>
 
-void behind(int *, int);
+void behind(int*, int);
 
 int main(void) {
     int array[10];
     int N, i;
-    
+
     scanf("%d", &N);
-    for (i=0; i<N; i++) {
+    for (i = 0; i < N; i++) {
         scanf("%d", &array[i]);
     }
     behind(array, N);
-    for (i=0; i<N; i++) {
+    for (i = 0; i < N; i++) {
         printf("%d\n", array[i]);
     }
-    
+
     return 0;
 }
 
 /* Write your function behind() here: */
 
 
-void behind(int *array, int N)
+void behind(int* array, int N)
 {
     int index = 0;
     int numBigger;
     //int positionNumBigger;
-     numBigger = 0;
-    while (index < N){
-       
+    numBigger = 0;
+    while (index < N) {
 
-        if(array[index] > numBigger){
+
+        if (array[index] > numBigger) {
             numBigger = array[index];
             //positionNumBigger = index;
         }
         index++;
     }
     index = 0;
-    while(index < N){
-        array[index] = numBigger - (array[index]) ;
-       
-    //printf(" %d\n", array[index]); 
-    
-    index++;
+    while (index < N) {
+        array[index] = numBigger - (array[index]);
+
+        //printf(" %d\n", array[index]); 
+
+        index++;
     }
-    
-    
+
+
 
 }
--- manipulacion array con punteros.c
+++ manipulacion array con punteros.c
@@ -5,21 +5,21 @@ Utilice un puntero para recorrer el array.
 Reemplace todos los valores negativos del array con el número 0, usando únicamente aritmética de punteros (es decir, no puedes usar índices de array como array[i]).
 Imprima el array modificado usando el puntero.*/
 
-    #include <stdio.h>
+#include <stdio.h>
 
 int main(void) {
-    int arr[] = {1, 2, -5, 6};
-    int *ptrArr = arr;
+    int arr[] = { 1, 2, -5, 6 };
+    int* ptrArr = arr;
     int size = sizeof(arr) / sizeof(arr[0]); // Calcular el tamaño del array
     int i = 0; // Contador para controlar el número de elementos
-    
+
     while (i < size) {
         if (*(ptrArr) < 0) {
             *(ptrArr) = 0;
         }
-        
+
         printf("%d ", *(ptrArr)); // Imprimir el valor del elemento actual
-        
+
         ptrArr++; // Avanzar al siguiente elemento
         i++; // Incrementar el contador
     }
