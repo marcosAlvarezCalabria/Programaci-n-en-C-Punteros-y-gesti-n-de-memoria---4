e7b7c19d3f289bae2fe847e743fe0005e8f07448
marcos <marcosalvarezcalabria@gmail.com>
2024-09-28 17:37:07 +0100

    activity allocate memory for streings fixed

--- a.c
+++ a.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-// Be sure to include any other library you may need...
+#include <stdlib.h>
 
-// Write your allocateString() prototype here
+char * allocateString(int numChars);// Write your allocateString() prototype here
 
 int main(void) {
     int lengthLight, lengthDark;
@@ -9,12 +9,14 @@ int main(void) {
     
     scanf("%d %d", &lengthLight, &lengthDark); 
     // Write a line of code here that calls the function allocateString(). 
+    strLight = allocateString(lengthLight);
     
     /* The goal is to reserve space for the light setting label, therefore you 
        need to pass the number lengthLight to the function allocateString()
        Store the return value of this function call in the variable strLight. */
      
     // Write a line of code here that calls the function allocateString().
+    strDark = allocateString(lengthDark); 
     
     /* This time the goal is to reserve space in memory for the dark setting label.
        Store the return value of the function call in the variable strDark. */
@@ -23,8 +25,11 @@ int main(void) {
     scanf("%s", strDark);
     printf("Local settings: %s - %s\n", strLight, strDark);
     // Write a line of code here to free the memory allocated for strLight
+    free(strLight);
     
     // Write a line of code here to free the memory allocated for strDark
+
+    free(strDark);
     
 	
     return 0;
@@ -32,8 +37,12 @@ int main(void) {
 
 char * allocateString(int numChars){
     // declare your variable(s) here
+
+    int num = numChars;
     
     // Write a line of code here that performs the memory allocation.
+   int  * ptr = (int *)  malloc(num * sizeof(int));
+
     /* You should allocate space in memory for the number of characters specified 
        via the input parameter to the function and the null terminator and store the 
        address of the allocated memory in a pointer named ptr. In order to receive credit 
